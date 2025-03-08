412eceda17b39c0c53ae4533e5aa6123d48a860d
Dhruv Parmar <35dhruvparmar@gmail.com>
2025-03-07 19:39:11 -0500

    Test Husky pre-commit hook

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -9,20 +9,18 @@ terraform {
   }
 }
 
-provider "azurerm" {
+            provider "azurerm" {
   features {}
 }
 
 resource "azurerm_resource_group" "example" {
   name     = "example-resources"
   location = "East US"
-}
 
-resource "azurerm_storage_account" "example" 
+
+                    resource "azurerm_storage_account" "example" 
   name                     = "examplestoracc"
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-)
-)
