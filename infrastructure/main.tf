7d56486b193d981a8cd30ac84b241db477e254cc
Dhruv Parmar <35dhruvparmar@gmail.com>
2025-03-07 19:02:03 -0500

    Test formatting error

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -24,4 +24,4 @@ resource "azurerm_storage_account" "example" {
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-}
+
